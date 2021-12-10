f63414bf52e33c6f9aeb887d2494c2326bf85576
rogerio pereira <rogeriopedramartins@gmail.com>
2021-12-10 02:09:48 -0300

    mudando componente de classe para hooks

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { useCallback, useEffect, useState } from "react";
 
 import "./styles.css";
 
@@ -7,77 +7,66 @@ import { loadPost } from "../../utils/load-post.js";
 import { Button } from "../../components/Button";
 import { TextInput } from "../../components/TextInput";
 
-class Home extends React.Component {
-  state = {
-    posts: [],
-    allPosts: [],
-    page: 0,
-    postPerPage: 10,
-    searchInput: "",
-  };
+export const Home = () => {
+  const [posts, setPosts] = useState([]);
+  const [allPosts, setAllPosts] = useState([]);
+  const [page, setPage] = useState(0);
+  const [postPerPage] = useState(10);
+  const [searchInput, setSearchInput] = useState("");
 
-  async componentDidMount() {
-    await this.loadPosts();
-  }
-
-  loadPosts = async () => {
-    const { page, postPerPage } = this.state;
-
+  const handleLoadPosts = useCallback(async (page, postPerPage) => {
     const postsandPhotos = await loadPost();
 
-    this.setState({
-      posts: postsandPhotos.slice(page, postPerPage),
-      allPosts: postsandPhotos,
-    });
-  };
+    setPosts(postsandPhotos.slice(page, postPerPage));
+    setAllPosts(postsandPhotos);
+  }, []);
 
-  loadMorePosts = () => {
-    const { page, postPerPage, allPosts, posts } = this.state;
+  useEffect(() => {
+    handleLoadPosts(0, postPerPage);
+  }, [handleLoadPosts, postPerPage]);
 
+  const loadMorePosts = () => {
     const nextPage = page + postPerPage;
     const nextPosts = allPosts.slice(nextPage, nextPage + postPerPage);
     posts.push(...nextPosts);
 
-    this.setState({ posts, page: nextPage });
+    setPosts(posts);
+    setPage(nextPage);
   };
 
-  handleInputSearch = (e) => {
+  const handleInputSearch = (e) => {
     const { value } = e.target;
-    this.setState({ searchInput: value });
+    setSearchInput(value);
   };
 
-  render() {
-    const { posts, page, postPerPage, allPosts, searchInput } = this.state;
+  const noMorePosts = page + postPerPage >= allPosts.length;
 
-    const noMorePosts = page + postPerPage >= allPosts.length;
+  const filterPosts = !!searchInput
+    ? allPosts.filter((post) => {
+        return post.title.toUpperCase().includes(searchInput.toUpperCase());
+      })
+    : posts;
 
-    const filterPosts = !!searchInput
-      ? allPosts.filter((post) => {
-          return post.title.toUpperCase().includes(searchInput.toUpperCase());
-        })
-      : posts;
+  return (
+    <section className={"container"}>
+      <div className="search-container">
+        <TextInput onChange={handleInputSearch} value={searchInput} />
+      </div>
 
-    return (
-      <section className={"container"}>
-        <div className="search-container">
-          <TextInput onChange={this.handleInputSearch} value={searchInput} />
-        </div>
+      {filterPosts.length > 0 && <Posts posts={filterPosts} />}
+      {filterPosts.length === 0 && <h1>Não esistem posts :(</h1>}
 
-        {filterPosts.length > 0 && <Posts posts={filterPosts} />}
-        {filterPosts.length === 0 && <h1>Não esistem posts :(</h1>}
-
-        <div className={"button-container"}>
-          {!searchInput && (
-            <Button
-              onClick={this.loadMorePosts}
-              text={"Load more Posts"}
-              disabled={noMorePosts}
-            />
-          )}
-        </div>
-      </section>
-    );
-  }
-}
+      <div className={"button-container"}>
+        {!searchInput && (
+          <Button
+            onClick={loadMorePosts}
+            text={"Load more Posts"}
+            disabled={noMorePosts}
+          />
+        )}
+      </div>
+    </section>
+  );
+};
 
 export default Home;
